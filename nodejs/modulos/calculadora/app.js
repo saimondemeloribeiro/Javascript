84e85c9938657f12403909d4a92a323be260f5ca
Saimon de Melo Ribeiro <Saimon de Melo Ribeiro>
2025-08-26 09:30:02 -0300

    versao_calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,13 @@
+const funcoesMatematica = require('./funcoes_matematica');
+
+// Exemplo de uso das funções importadas
+//const resultadoSoma = funcoesMatematica.soma(5, 3);
+
+console.log('Soma:', funcoesMatematica.soma(5, 3));
+
+console.log('Subtração:', funcoesMatematica.subtrai(10, 4));
+
+console.log('Multiplicação:', funcoesMatematica.multiplica(9, 9));
+
+console.log('Divisão:', funcoesMatematica.divide(20, 2));
+
---
+++ nodejs/modulos/calculadora/funcoes_matematica.js
@@ -0,0 +1,22 @@
+function soma(a, b) {
+    return a + b;
+}
+
+function subtrai(a, b) {
+    return a - b;
+}
+
+function multiplica(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+module.exports = {
+    soma,
+    subtrai,
+    multiplica,
+    divide
+};
