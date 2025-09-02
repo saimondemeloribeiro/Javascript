18cd5b4dd9754c4427936103b18fb89ba6a57a8c
Saimon de Melo Ribeiro <Saimon de Melo Ribeiro>
2025-09-02 09:26:22 -0300

    versao fs os

--- nodejs/modulos/app.js
+++
@@ -1,4 +0,0 @@
-const saudacao = require('./meuModulo'); // Importando o módulo
-
-const mensagem = saudacao('Saimon'); // Executando a função
-console.log(mensagem);
---
+++ nodejs/modulos/fs/app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('exemplo.txt', 'utf8', (erro, conteudo_do_arquivo) => {
+if (erro) {
+    console.error('Erro ao ler o arquivo:', erro);
+    return;
+}
+    console.log('Conteúdo do arquivo:', conteudo_do_arquivo);
+});
+console.log('Esta mensagem aparece primeiro.');
